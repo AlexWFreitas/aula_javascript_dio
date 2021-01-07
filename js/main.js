ebc787cba0e99561a01b7123414fd83ebbfaa281
Alex Wong de Freitas <otaru1921@gmail.com>
2021-01-06 23:27:00 -0300

    First lesson

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+var nome = "Alex Wong de Freitas";
+var idade = 32;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+console.log(nome + " tem " + idade + " anos");
+console.log(idade * idade2);
+console.log(frase);
+alert(frase.replace("Japão", "Brasil"));
+
